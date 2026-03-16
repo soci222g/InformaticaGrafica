93e8d58ad46b1321fe781c3b2196e14310057a42
Jordi Gatacos <jordi.gratacos@estudiant.enti.cat>
2026-03-16 18:03:33 +0100

    codi de crea un puntito

--- MyFirstOpenGL/MyFirstOpenGL/Source.cpp
+++ MyFirstOpenGL/MyFirstOpenGL/Source.cpp
@@ -5,122 +5,136 @@
 #define WINDOW_WIDTH 640
 #define WINDOW_HEIGHT 480
 
-void Resize_Window(GLFWwindow* window, int iFrameBufferWidth, int iFrameBufferHeight) {
 
-	//Definir nuevo tamaño del viewport
-	glViewport(0, 0, iFrameBufferWidth, iFrameBufferHeight);
+void ResizeWindow(GLFWwindow* window, int iNewFrameBufferWidth, int iNewFrameBufferHeight) {
+	//definir nou tamany
+	glViewport(0, 0, iNewFrameBufferWidth, iNewFrameBufferHeight);
+
 }
+void main() {
 
-void main()
-{
-	int iFrameBufferWidth = 0;
-	int iFrameBufferHeight = 0;
 
-	//Inicializamos GLFW para gestionar ventanas e inputs
+
+
+	//INICIALITZA GLFW per gestiona finestres
 	glfwInit();
 
-	//Configuramos la ventana
+
+
+	//pillar drivers
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
-	glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
 
-	//Inicializamos la ventana
-	GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "My Engine", NULL, NULL);
+	//vercio principal AKA OPenGl 4.4
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR,4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR,4);
 
-	//Asignamos función de callback para cuando el frame buffer es modificado
-	glfwSetFramebufferSizeCallback(window, Resize_Window);
+	glfwWindowHint(GLFW_RESIZABLE, GL_TRUE); //configura si es escalabla la finestra
 
-	//Definimos espacio de trabajo
+
+
+	GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "WINDOW1", NULL, NULL);
+
+
+	glfwSetFramebufferSizeCallback(window, ResizeWindow);
+	
+	//definim la finestra
 	glfwMakeContextCurrent(window);
 
-	//Permitimos a GLEW usar funcionalidades experimentales
+
+	//activem funcions experimentals per a totes les grafiques
 	glewExperimental = GL_TRUE;
 
-	//Activamos cull face
-	glEnable(GL_CULL_FACE);
 
-	//Indicamos lado del culling
-	glCullFace(GL_BACK);
 
-	//Inicializamos GLEW y controlamos errores
 	if (glewInit() == GLEW_OK) {
-
-		//Definimos color para limpiar el buffer de color
+		std::cout << "ha funcionat" << std::endl;
+		
+		//set el color del buffer  (el de darrera)
 		glClearColor(1.f, 0.f, 0.f, 1.f);
+
+
+
+
 
 		GLuint vaoPuntos, vboPuntos;
 
-		//Definimos cantidad de vao a crear y donde almacenarlos 
+
+		//Generop el VAO i m'el Guardo a vaoPuntos
 		glGenVertexArrays(1, &vaoPuntos);
 
-		//Indico que el VAO activo de la GPU es el que acabo de crear
+		//activo el vao per trevalla en ell
 		glBindVertexArray(vaoPuntos);
 
-		//Definimos cantidad de vbo a crear y donde almacenarlos
+
+		//amb generas un vbo i m'el guardes.
 		glGenBuffers(1, &vboPuntos);
 
-		//Indico que el VBO activo es el que acabo de crear y que almacenará un array. Todos los VBO que genere se asignaran al último VAO que he hecho glBindVertexArray
-		glBindBuffer(GL_ARRAY_BUFFER, vboPuntos);		
+		//indico qun VBO es el actiu i que esta gaurdan arrays de dades
+		glBindBuffer(GL_ARRAY_BUFFER, vboPuntos);
 
-		//Posición X e Y del punto
-		GLfloat punto[] = {
-			-0.5f,  0.5f, // Vértice superior izquierdo
-			-0.5f, -0.5f, // Vértice superior derecho
-			 0.0f,  0.5f, // Vértice inferior derecho
-			 0.0f, -0.5f, // Vértice inferior derecho
-			 0.5f,  0.5f, // Vértice inferior izquierdo
-			 0.5f, -0.5f  // Vértice superior izquierdo
-		};
 
-		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
-		//Ponemos los valores en el VBO creado
-		glBufferData(GL_ARRAY_BUFFER, sizeof(punto), punto, GL_STATIC_DRAW);
+		//declarem un punt en el x i y
+		GLfloat puntos[] = { 0.f,0.f };
 
-		//Indicamos donde almacenar y como esta distribuida la información
+
+		//posu el array en el VBO
+		glBufferData(GL_ARRAY_BUFFER, sizeof(puntos), puntos, GL_STATIC_DRAW);
+
+
+		//definicm com llegir la info del VBO, 
 		glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(GLfloat), (GLvoid*)0);
 
-		//Indicamos que la tarjeta gráfica puede usar el atributo 0
+
+		//acties les dades de la gpu que els pugui utilitza
 		glEnableVertexAttribArray(0);
 
-		//Desvinculamos VAO
-		glBindVertexArray(0);
-
-		//Desvinculamos VBO
 		glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-		//Generamos el game loop
-		while (!glfwWindowShouldClose(window)) {
+		glBindVertexArray(0);
 
-			//Pulleamos los eventos (botones, teclas, mouse...)
+		while (!glfwWindowShouldClose(window))
+		{
+			//fem un pull de events
 			glfwPollEvents();
 
-			//Limpiamos los buffers
-			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
 
-			//Definimos que queremos usar el VAO con los puntos
+		
+			//clear buffers
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+			
+
+
+			//cridem a la geometria del VAO ( el VBO esta dintre de aquest)
 			glBindVertexArray(vaoPuntos);
 
-			//Definimos que queremos dibujar
-			glDrawArrays(GL_TRIANGLE_STRIP, 0, 6);
+			//definim quiona info estem pintan del vao (en aquest cas punts , des del element 0 fins el 1)
+			glDrawArrays(GL_POINTS, 0, 1);
 
-			//Dejamos de usar el VAO indicado anteriormente
+			//desativem el VAO
 			glBindVertexArray(0);
 
-			//Cambiamos buffers
+			//preven la dessincruintzacio de FPS amb Hz
 			glFlush();
+
+			//fas swap dels buffers
 			glfwSwapBuffers(window);
+
 		}
 
-	}else {
-		std::cout << "Ha petao." << std::endl;
+	}
+	else {
+		std::cout << "ha muerto" << std::endl;
 		glfwTerminate();
 	}
-	//Finalizamos GLFW
-	glfwTerminate();
-}
 
+
+
+	glfwTerminate();
+
+
+
+}
 
 
 
